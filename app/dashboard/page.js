775df4365237c1214f7f12d5cfac0d2376bc7143
Prashant <goden1029@gmail.com>
2024-05-25 16:58:35 +0530

    resolve error

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+"use client";
+const Dashboard = () => {
+    // Your page content here
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {/* Other components or content */}
+        </div>
+    );
+};
+
+export default Dashboard;
